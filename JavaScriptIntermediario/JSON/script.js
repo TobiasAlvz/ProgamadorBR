0a0d0b7a08f981e924d4aca6bfa52b6b8f7ce708
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2022-11-08 09:44:24 -0300

    up

--- JavaScriptIntermediario/JSON/script.js
+++ JavaScriptIntermediario/JSON/script.js
@@ -8,3 +8,10 @@ console.log(a);
 // JavaScript Object notation - forma de representar um objeto javascript
 let b = JSON.stringify(a);
 console.log(b);
+
+let a2 = '{"nome": "Ana", "nota":9}';
+
+let b2 = JSON.parse(a2);
+
+console.log(a2);
+console.log(b2);
