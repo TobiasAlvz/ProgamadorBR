e3b5f7b306894f11f16805f2cec06ad2b3755403
TobiasAlvz <102394909+TobiasAlvz@users.noreply.github.com>
2022-11-14 09:44:58 -0300

    up

---
+++ JavaScriptBasico/null-Undefined/script.js
@@ -0,0 +1,5 @@
+// Null: temos um espaço para ser colocado o recipiente, mas há nada nele.
+
+// Undefined: nada sequer foi mencionado para ser colocado nesse lugar, nunca houve uma cafeteira, ou seja, indefinido.
+
+// Zero: não há café, mas temos a cafeteira somente para ele, junto do recipiente vazio.
